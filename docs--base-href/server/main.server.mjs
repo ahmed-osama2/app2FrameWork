0f19173ec0f79443f26312ada6069f129362546e
ahmed-osama2 <ahmedosama12467@gmail.com>
2024-08-09 00:31:15 +0300

    first commit

---
+++ docs--base-href/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-VARBBAXG.mjs";import"./chunk-TZE44NSG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'Start Framework',pathMatch:'full'},
+    { path: 'Start Framework', component: HomeComponent, title: 'home page' },
+    { path: 'ABOUT', component: AboutComponent, title: 'about page' },
+    { path: 'PORTFOLIO', component: PortfolioComponent, title: 'portflio' },
+    { path: 'CONTACT', component: ContactComponent, title: 'contact page' },
+    {path:'**',component: NotfoundComponent,title: 'Not found'}
+    
+]
